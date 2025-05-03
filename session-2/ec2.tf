31edea3c4649fd69a15861eb8297f95307349b53
Aigerim2024 <llc.sfdev@gmail.com>
2025-05-03 17:57:23 -0500

    added

--- session-2/ec2.tf
+++ session-2/ec2.tf
@@ -2,47 +2,47 @@
 
 
 
-resource "aws_instance" "first_ec2" {
-  ami           = "ami-07b0c09aab6e66ee9" # argument
-  instance_type = "t2.micro"
-  tags = {
-    Name        = "aws-session2-instance"
-    Environment = "dev"
-  }
-}
-
-resource "aws_instance" "second_ec2" {
-  ami           = "ami-07b0c09aab6e66ee9" # argument
-  instance_type = "t2.micro"
-  tags = {
-    Name        = "aws-session2-instance"
-    Environment = "dev"
-  }
-}
-
-
-resource "aws_security_group" "simple_sg" {
-  name = "simple-sg"
-  description = "this is a test simple-sg "
-
-  ingress {
-
-    from_port       = 22    // Number does not use ""
-    to_port         = 22
-    protocol        = "tcp"  // everything inside "" string
-    cidr_blocks = ["0.0.0.0/0"] // List of String block"S"
+# resource "aws_instance" "first_ec2" {
+#   ami           = "ami-07b0c09aab6e66ee9" # argument
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name        = "my_instance"
+#     Environment = "dev"
+#   }
   
-  }
-  egress {
+
+# resource "aws_instance" "second_ec2" {
+#   ami           = "ami-07b0c09aab6e66ee9" # argument
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name        = "aws-session2-instance"
+#     Environment = "dev"
+#   }
+# }
+
+# }
+# resource "aws_security_group" "simple_sg" {
+#   name = "simple-sg"
+#   description = "this is a test simple-sg "
+
+#   ingress {
+
+#     from_port       = 22    // Number does not use ""
+#     to_port         = 22
+#     protocol        = "tcp"  // everything inside "" string
+#     cidr_blocks = ["0.0.0.0/0"] // List of String block"S"
+  
+#   }
+#   egress {
     
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"  // tcp,udp,icmp
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#     from_port       = 0
+#     to_port         = 0
+#     protocol        = "-1"  // tcp,udp,icmp
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
   
-}
+# }
 
 
 # Block and Argument
--- session-2/homework.tf
+++ session-2/homework.tf
@@ -20,20 +20,20 @@ resource "aws_instance" "first_ec2" {
 }
 
 resource "aws_security_group" "simple_sg" {
-  name        = "simple-sg"
-  description = "this is a test simple-sg"
+   name        = "simple-sg"
+   description = "this is a test simple-sg"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+   ingress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+   egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+ }
